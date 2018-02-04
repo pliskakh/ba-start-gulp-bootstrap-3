e80850b84345d10c05183460e782a5a3ad59d22c
Mykhailo Pliskunov <pliskunov.mykhailo@gmail.com>
2018-02-04 14:33:06 +0200

    init

---
+++ src/assets/js/app.js
@@ -0,0 +1,42 @@
+//=require jquery.min.js
+
+// to inlude bootstrap plugins add "=" sign below
+// require javascripts/bootstrap/*.*
+// require javascripts/bootstrap.min.js
+
+//=require slick.min.js
+
+;(function($){
+
+	$(document).ready(function(){
+		//var $slider = $('.ba-slider');
+
+		// $slider.slick({
+		// 	dots: true,
+		// 	infinite: false
+		// });
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
